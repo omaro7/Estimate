7aae4f035702aada5097cceece4a2cea5977f394
JUNGHOON HAN <omaro7@hanmail.net>
2024-01-15 09:39:44 +0900

    다음 주소api 수정 ㄴ 등록된 applicaiton id에서만 접속 가능하도록 param 추가

--- app/src/main/java/kr/co/goms/app/estimate/activity/AddressApiActivity.java
+++ app/src/main/java/kr/co/goms/app/estimate/activity/AddressApiActivity.java
@@ -1,10 +1,20 @@
 package kr.co.goms.app.estimate.activity;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 import kr.co.goms.app.estimate.BuildConfig;
 import kr.co.goms.app.estimate.R;
@@ -28,6 +38,7 @@ public class AddressApiActivity extends CustomActivity {
             finish();
         }
     }
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,11 +51,37 @@ public class AddressApiActivity extends CustomActivity {
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                webView.loadUrl("javascript:execDaumPostCode();");
+                //webView.loadUrl("javascript:execDaumPostCode();");
             }
         });
 
-        webView.loadUrl(BuildConfig.ADDRESS_SERVER_URL);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                // Handle the JavaScript alert here
+                // You can display a dialog or perform any custom action
+                // For example, showing an AlertDialog:
+                new AlertDialog.Builder(AddressApiActivity.this)
+                        .setTitle("알림")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok, (dialog, which) -> result.confirm())
+                        .setCancelable(false)
+                        .show();
+
+                // Return true to indicate that you've handled the alert
+                return true;
+            }
+        });
+
+        // The data you want to send in the request body
+        byte[] value = Base64.encode(BuildConfig.APPLICATION_ID.getBytes(), Base64.DEFAULT);   //kr.co.goms.app.estimate
+        String postData = "key=" + new String(value);
+
+        Log.d("POST", "postData : " + postData);
+
+        // Convert the data to bytes
+        byte[] postDataBytes = postData.getBytes(StandardCharsets.UTF_8);
+        webView.postUrl(BuildConfig.ADDRESS_SERVER_URL, postDataBytes);
 
     }
 
